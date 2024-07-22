2de5b80f77aa49f6e7680fa1611534f93d0cb8a7
Дарья Захарова <dasha05z@yandex.ru>
2024-07-22 18:58:06 +0300

    Done test_content pytest and unittest.

--- notes/tests/test_content.py
+++ notes/tests/test_content.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
+from notes.models import Note
 from notes.forms import NoteForm
 
 
@@ -12,12 +13,37 @@ class TestContent(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.author = User.objects.create(username='Someone')
-        cls.url_add = reverse('notes:add', args=None)
+        cls.author = User.objects.create(username='Writer')
+        cls.anotheruser = User.objects.create(username='Someoneelse')
+        cls.slug_name = 'helping'
+        cls.note = Note.objects.create(
+            title='Название',
+            text='Текст',
+            slug=cls.slug_name,
+            author=cls.author
+        )
 
-    def test_authorized_client_has_form(self):
+    def test_pages_contains_form(self):
+        urls = (
+            ('notes:add', None),
+            ('notes:edit', (self.slug_name,))
+        )
         self.client.force_login(self.author)
-        response = self.client.get(self.url_add)
-        self.assertIn('form', response.context)
-        self.assertIsInstance(response.context['form'], NoteForm)
-    # Для анонимного с 'form' не работает, а проверка на редирект уже была
+        for name, arg in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=arg)
+                response = self.client.get(url)
+                self.assertIn('form', response.context)
+                self.assertIsInstance(response.context['form'], NoteForm)
+
+    def test_notes_list_for_different_users(self):
+        url = reverse('notes:list')
+        users = (
+            (self.author, True),
+            (self.anotheruser, False)
+        )
+        for user, bool_stat in users:
+            self.client.force_login(user)
+            response = self.client.get(url)
+            object_list = response.context['object_list']
+        self.assertEqual(bool(self.note in object_list), bool_stat)
